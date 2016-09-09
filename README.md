ae29c66e9457f24265266f54ee2b43a2d38b7dd3
Jon Austen <jon.austen@healthsparq.com>
2016-09-09 16:49:30 -0700

    Minor edits.

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ Parent project at http://selenide.org/
 
 To run, execute `gradle clean test`
 
-Configure Chrome driver location in build.gradle as a system property  (or alternatively, build a factory class)
+Configure Chrome driver location in build.gradle as a system properties (or alternatively, 
+build a factory class).  I include a Chrome driver binary in thsi project but you might need to update it.
 
 
 
--- test/GoogleTest.java
+++ test/GoogleTest.java
@@ -5,12 +5,15 @@ import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
-public class GoogleTest {
+public class GoogleTest
+{
     @Test
-    public void search_selenide_in_google() {
+    public void search_selenide_in_google()
+    {
         open("http://google.com");
         $(By.name("q")).val("selenide").pressEnter();
         $$("#ires li.g").shouldHave(size(10));
         $("#ires").find(By.linkText("selenide.org")).shouldBe(visible);
     }
+
 }
